f218a4af11c54bd8266dc3af1a6c574a783549f8
itsrusty <rusty3031@gmail.com>
2023-10-30 00:37:57 -0700

    implement a joi validation data in schemas mongoose for more protection!

--- src/server/index.js
+++ src/server/index.js
@@ -2,18 +2,24 @@ import express from "express"
 import morgan from "morgan"
 import dotenv from "dotenv"
 import cors from "cors"
+import { connectionDB } from "../config/connection_db.js"
+import routerPermissionsUser from "../routes/users/authentication.js"
 import colors from "colors"
 
+connectionDB()
 dotenv.config()
 
 const PORT = process.env.PORT
 const server = express()
 
 // ? middlewares
+server.use(express.urlencoded({ extended: true }));
 server.use(morgan("dev"))
+server.use(express.json())
 server.use(cors())
 
 // ? endpoints
+server.use(routerPermissionsUser)
 
 const serverListening = () => {
     try {
